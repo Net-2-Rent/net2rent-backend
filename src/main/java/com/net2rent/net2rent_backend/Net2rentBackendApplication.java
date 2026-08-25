c50ce75c60f2c8cca78fb61415303ceaff9e68c2
Johanna Monroy <johamonroy20@gmail.com>
2026-08-25 10:00:57 +0200

    first commit

---
+++ src/main/java/com/net2rent/net2rent_backend/Net2rentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.net2rent.net2rent_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Net2rentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Net2rentBackendApplication.class, args);
+	}
+
+}
