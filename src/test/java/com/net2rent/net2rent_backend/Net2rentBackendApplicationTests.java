a0af4aad0eb46fa5f9777fdebaed534d84a240a9
Johanna Monroy <johamonroy20@gmail.com>
2026-08-26 12:19:36 +0200

    first commit (#1)

---
+++ src/test/java/com/net2rent/net2rent_backend/Net2rentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.net2rent.net2rent_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Net2rentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
