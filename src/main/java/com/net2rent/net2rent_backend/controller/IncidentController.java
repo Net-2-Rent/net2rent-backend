8b8d734c5b38e7323480a8f341c717546ddc6ad9
Johanna Monroy <johamonroy20@gmail.com>
2026-09-07 13:35:33 +0200

    edit compilation

--- src/main/java/com/net2rent/net2rent_backend/controller/IncidentController.java
+++ src/main/java/com/net2rent/net2rent_backend/controller/IncidentController.java
@@ -81,7 +81,7 @@ public class IncidentController {
             @PathVariable Long id,
             @AuthenticationPrincipal GuestPrincipal guest) {
         return GuestIncidentDetailResponse.from(
-                incidentService.getOwnedByLodgingOr404(id, guest.LodgingId()));
+                incidentService.getOwnedByLodgingOr404(id, guest.lodgingId()));
     }
 
     @PostMapping
